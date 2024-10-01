d661f1b63f9ae5363b37ff89da54df19a32dc516
Guilherme Lima <enriqueguilherme1209@gmail.com>
2024-10-01 18:53:33 -0300

    adicao das funcoes, match-case, importacao e definicao do arquivo main

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-print ("""
+import os
+
+def exibir_nome_do_programa():
+    print ("""
 
 ██████╗░██╗██████╗░███████╗████████╗░█████╗░░█████╗░██╗
 ██╔══██╗██║██╔══██╗██╔════╝╚══██╔══╝██╔══██╗██╔══██╗██║
@@ -7,12 +10,40 @@ print ("""
 ██████╔╝██║██║░░██║███████╗░░░██║░░░╚█████╔╝██║░░██║██║
 ╚═════╝░╚═╝╚═╝░░╚═╝╚══════╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝╚═╝
        
-       """)
-print("1. Cadastrar restaurante")
-print("2. Listar restaurante")
-print("3. Ativar restaurante")
-print("4. Sair\n")
+""")
+    
+def exibir_opcoes():
+    print("1. Cadastrar restaurante")
+    print("2. Listar restaurante")
+    print("3. Ativar restaurante")
+    print("4. Sair\n")
 
-opcao_escolhida = 4
+def finalizar_app():
+    os.system("cls")
+    print("Finalizando o app\n")
 
-print(f"Você escolheu a opção{opcao_escolhida}")
+def escolher_opcoes():
+    opcao_escolhida = int(input("Escolha uma opção: "))
+    match opcao_escolhida:
+        case 1:
+            print("Cadastrar restaurante")
+        case 2:
+            print("Listar restaurantes")
+        case 3:
+            print("Ativar restaurante")
+        case 4:
+            finalizar_app()
+        case _:
+            print("Opção inválida")
+            
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcoes()
+
+
+if __name__ == "__name__":
+    main()
+
+main()
